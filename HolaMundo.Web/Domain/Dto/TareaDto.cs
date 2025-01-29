cba91c8361b24b68f5ddbd36357b6cbef169df8a
Felix Benzan <felixbenz07@gmail.com>
2025-01-28 21:10:39 -0400

    Agregar archivos de proyecto.

---
+++ HolaMundo.Web/Domain/Dto/TareaDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HolaMundo.Web.Domain.Dto;
+
+public class TareaDto
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "El nombre de la tarea es obligatorio")]
+    public string Nombre { get; set; }
+    public DateTime? Fecha { get; set; }
+}
