2dd9d316d6600414698c0d17886098598e9f6516
Felix Benzan <felixbenz07@gmail.com>
2025-02-18 20:56:27 -0400

    Configuro la conexion con la base de datos, utilizando el appSettings de mi aplicacion.

--- ERP.Web/Program.cs
+++ ERP.Web/Program.cs
@@ -1,10 +1,14 @@
 using ERP.Web.Components;
+using ERP.Web.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+//Data Services
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
