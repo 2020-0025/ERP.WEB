91a0f4624a1ee2dd405e380d1144b6a1f7c6c146
Felix Benzan <felixbenz07@gmail.com>
2025-02-25 21:02:06 -0400

    Añadir gestión de clientes y enlace en el menú
    Se ha añadido un nuevo enlace en el menú de navegación (`NavMenu.razor`) para la página de "Clientes".
    Se ha importado el espacio de nombres `ERP.Web.Domain.Dto` en `_Imports.razor`.
    En `PersonaDto.cs`, se ha añadido la clase `ClienteDto` que incluye propiedades como `Id`, `PersonaId`, `LimiteDeCredito` y `DatosPersonales`.
    Se ha creado una nueva página `Clientes.razor` que permite gestionar clientes. Esta página incluye:
      * Un formulario para agregar o modificar clientes, con campos para el nombre, fecha de nacimiento y límite de crédito.
      * Una tabla que lista los clientes con opciones para modificar o eliminar cada cliente.
      * Métodos en el bloque `@code` para manejar la lógica de agregar, modificar y limpiar el formulario.

--- ERP.Web/Domain/Dto/PersonaDto.cs
+++ ERP.Web/Domain/Dto/PersonaDto.cs
@@ -1,6 +1,18 @@
-﻿namespace ERP.Web.Domain.Dto
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERP.Web.Domain.Dto
 {
     public class PersonaDto
     {
+        public int Id { get; set; }//No requerir (Generado por la DB)
+        public string Nombre { get; set; } = null!; //Si
+        public DateTime? FechaDeNacimiento { get; set; }//Si
+    }
+    public class ClienteDto
+    {
+        public int Id { get; set; }//No requerir (Generado por la DB)
+        public int PersonaId { get; set; }//No requerir (Generado por la DB)
+        public decimal LimiteDeCredito { get; set; }//Si
+        public PersonaDto DatosPersonales { get; set; } = new PersonaDto();
     }
 }
