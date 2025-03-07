b83727137f4dbc04de0d6a7b69755ae5a1f48708
2020-0025 <2020-0025@uteco.edu.do>
2025-03-06 20:44:20 -0400

    Inicie el servicio y la pagina de empleados

---
+++ ERP.Web/Services/EmpleadoService.cs
@@ -0,0 +1,42 @@
+﻿using ERP.Web.Data;
+using ERP.Web.Domain.Dto;
+using ERP.Web.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace ERP.Web.Services
+{
+    public class EmpleadoService
+    {
+        private readonly AppDbContext _context;
+
+        public EmpleadoService(AppDbContext context)
+        {
+            _context = context;
+        }
+        ///Consultar los Empleados existentes
+        public async Task<List<EmpleadoDto>> Consultar(string filtro)
+        {
+            var empleados = await
+                _context.Empleados
+                .Include(c => c.DatosPersonales)
+                .Where(c => c.DatosPersonales.Nombre.Contains(filtro))
+                .Select(
+                    c =>
+                    new EmpleadoDto()
+                    {
+                        Id = c.Id,
+                        PersonaId = c.PersonaId,
+                        Sueldo = c.Sueldo,
+                        DatosPersonales = new PersonaDto()
+                        {
+                            Id = c.DatosPersonales.Id,
+                            Nombre = c.DatosPersonales.Nombre,
+                            FechaDeNacimiento = c.DatosPersonales.FechaDeNacimiento
+                        }
+                    }
+                )
+                .ToListAsync();
+            return empleados;
+        }
+
+    }
+}
